e157e107d1811c6842cec226e86bc0ebdef417c3
kens <tarotarohanak@gmail.com>
2025-01-16 22:36:56 +0900

    盤面表示追加

---
+++ README.md
@@ -0,0 +1,29 @@
+## Node.js とパッケージのインストール方法
+
+以下のコマンドで、.tool-versions に書かれたバージョンの Node.js をインストールしてください。
+
+```console
+asdf install
+```
+
+以下のコマンドで、package.json に書かれたライブラリをインストールしてください。
+
+```console
+npm install
+```
+
+## .ts ファイルの実行方法
+
+以下のようにして、.ts ファイルを実行することができます。
+
+```console
+npx ts-node hello.ts
+```
+
+## リバーシアプリケーションの開発環境の起動
+
+以下のコマンドで、開発用にホットリロードを有効にして Express を起動できます。
+
+```console
+npm start
+```
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,38 @@
-import express from "express"
-import morgan from "morgan"
+import express from 'express'
 import 'express-async-errors'
+import morgan from 'morgan'
 
 const PORT = 3000
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(express.static('static', { extensions: ['html'] }))
 
 app.get('/api/hello', async (req, res) => {
-    res.json({
-        message: 'Hello Express'
-    })
+  res.json({
+    message: 'Hello Express!!!'
+  })
 })
 
 app.get('/api/error', async (req, res) => {
-    throw new Error('Error endpoint')
+  throw new Error('Error endpoint')
 })
 
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log('Reversi application started: http:')
+  console.log(`Reversi application started: http://localhost:${PORT}`)
 })
 
-function errorHandler(err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) {
-    console.error('Unexpected error occurred', err)
-    res.status(500).send({
-        message: 'Unexpected error occurred'
-    })
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error('Unexpected error occurred', err)
+  res.status(500).send({
+    message: 'Unexpected error occurred'
+  })
 }
